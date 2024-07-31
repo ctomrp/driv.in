6a3d1b4bc65c4fb51c8befbd1270b4a72af538a7
ctomrp <cristian.rico.palma@gmail.com>
2024-07-31 07:25:55 -0400

    fix: map marker

--- src/components/MapViewComponent/MapViewComponent.jsx
+++ src/components/MapViewComponent/MapViewComponent.jsx
@@ -37,7 +37,7 @@ function MapViewComponent() {
 
   useEffect(() => {
     initializeMap();
-  }, [latitude, longitude]);
+  }, [latitude, longitude]); // Asegúrate de que la función useEffect tenga las dependencias adecuadas
 
   return (
     <div className='flex flex-col min-h-screen bg-[#001E2C] text-white'>
@@ -49,13 +49,13 @@ function MapViewComponent() {
       </div>
       <div className="flex flex-col mx-auto">
         <div className="text-lg pb-5">
-        El vehículo <span className="capitalize">{car.make}</span>&nbsp;
-        modelo <span className="capitalize">{car.model}</span>&nbsp;
-        del año {car.year} se encuentra en:          
+          El vehículo <span className="capitalize">{car.make}</span>&nbsp;
+          modelo <span className="capitalize">{car.model}</span>&nbsp;
+          del año {car.year} se encuentra en:          
         </div>
-      <div className="flex justify-center">
-        <div id="map" style={{ height: '50vh', width: '50vw' }}></div>
-      </div>
+        <div className="flex justify-center">
+          <div id="map" style={{ height: '50vh', width: '50vw' }}></div>
+        </div>
       </div>
     </div>
   );
